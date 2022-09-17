66a9fa3e80933e69095096ee3e0cd6859a3e0dca
Lia Pires <liasth.pires@gmail.com>
2022-09-17 16:56:59 -0300

    Adding mock posts
    Adding mock posts

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Lia Pires',
+        'title': 'Blog Post 1',
+        'content': 'This is my first blog post',
+        'date_posted': '16th September, 2022',
+    },
+
+    {
+        'author': 'Pia Lires',
+        'title': 'Blog Post 2',
+        'content': 'This is my blog post',
+        'date_posted': '17th September, 2022',
+    }
+
+
+]
+
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
 
 def about(request):
     return render(request, 'blog/about.html')
