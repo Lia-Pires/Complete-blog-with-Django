d66a0aa41dd8c5123313f29bfd5b662c769a0a8c
Lia Pires <liasth.pires@gmail.com>
2022-10-02 17:35:24 -0300

    Update Blog Project
    Adding Data Base

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,28 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-posts = [
-    {
-        'author': 'Lia Pires',
-        'title': 'Blog Post 1',
-        'content': 'This is my first blog post',
-        'date_posted': '16th September, 2022',
-    },
-
-    {
-        'author': 'Pia Lires',
-        'title': 'Blog Post 2',
-        'content': 'This is my blog post',
-        'date_posted': '17th September, 2022',
-    }
-
-
-]
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
