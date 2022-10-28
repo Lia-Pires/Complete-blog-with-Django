3790f3adc84cd7a4999f496e740bc77ada2746a6
Lia Pires <liasth.pires@gmail.com>
2022-10-28 17:35:04 -0300

    Update Views.py

--- blog_project/users/views.py
+++ blog_project/users/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserRegisterForm
 
+
 def register(request):
     if request.method == "POST":
         form = UserRegisterForm(request.POST)
